20e8db00423c8adf3b3ec887bc5e5d7415d921c3
jbock <jbock-java@gmx.de>
2024-12-31 18:12:00 +0100

    split class into header file

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
+#include "parser.hpp"
 
-class Parser {
-
-  public: static void consume_whitespace(std::istream* in) {
-    while(true) {
-      char c = in->peek();
-      if (c != ' ') {
-        return;
-      }
-      in->get();
+void Parser::consume_whitespace(std::istream* in) {
+ while(true) {
+    char c = in->peek();
+    if (c != ' ') {
+      return;
     }
+    in->get();
   }
-};
-
+}
---
+++ src/parser.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+class Parser {
+
+  public:
+    static void consume_whitespace(std::istream* in);
+};
+
