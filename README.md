3334b9d460d16099c1d253d06a2de13c0988f139
jbock <jbock-java@gmx.de>
2024-12-31 12:14:30 +0100

    read from stdin

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to test
+
+```sh
+make && ./clops <<< ab
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main(int argc, const char* argv[]) {
   cout << "Hello World!" << endl;
+  char c;
+  c = cin.peek();
+  cout << c << endl;
+  c = cin.get();
+  cout << c << endl;
+  c = cin.get();
+  cout << c << endl;
   return 0;
 }
